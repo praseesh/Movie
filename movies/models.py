85e32698b3d2b47ec731a9fddb6ee510b21cd3f1
praseesh <praseeshprasee@gmail.com>
2024-02-08 12:11:26 +0530

    new class created

--- movies/models.py
+++ movies/models.py
@@ -9,12 +9,21 @@ class Director(models.Model):
     def __str__(self):
         return self.name
     
+class Actor(models.Model):
+    name=models.CharField(max_length=300) 
+
+    def __str__(self):
+        return self.name
+
 class CensorInfo(models.Model):
     rating = models.CharField(max_length =10, null = True )
     certified_by = models.CharField(max_length =200, null = True )
 
+    def __str__(self):
+        return self.certified_by
 
-class MovieInfo (models.Model):
+
+class MovieInfo(models.Model):
     title=models.CharField(max_length=250)
     year=models.IntegerField(null=True)
     description=models.TextField()
@@ -26,7 +35,16 @@ class MovieInfo (models.Model):
     on_delete=models.SET_NULL,
     related_name='movie'
 )
-
+    directed_by=models.ForeignKey(
+        Director,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='directed_movies'   
+)
+    actors=models.ManyToManyField(
+        Actor,
+        related_name='acted_movies'
+    )
 
     def __str__(self):
         return self.title
